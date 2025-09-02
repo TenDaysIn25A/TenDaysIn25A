d23077ea80c13cc1f366f07c5d198ab56e7814cb
daichi.kujata@gmail.com <daichi.kujata@gmail.com>
2025-09-02 21:29:11 +0900

    add/GameScene

---
+++ TenDaysGameJam25A/GameScene.cpp
@@ -0,0 +1,70 @@
+﻿#include "GameScene.h"
+
+GameScene::GameScene() { Initialize(); }
+
+void GameScene::Initialize() {
+
+	player.Initialize();
+
+	enemy.Initialize();
+
+	currentDimension = DimensionState::TWO;
+}
+
+void GameScene::Update() {
+	// ここで各更新処理を行う
+
+	input.Update();
+
+	if (input.GetKeyTrigger(DIK_0)) {
+		if (currentDimension == DimensionState::ONE) {
+			currentDimension = DimensionState::TWO;
+		} else {
+			currentDimension = DimensionState::ONE;
+		}
+	}
+
+	enemy.Update();
+
+	player.Update();
+	
+	SetCamera();
+
+}
+
+void GameScene::Draw() const {
+
+}
+
+void GameScene::SetCamera() {
+	player.SetCamera(camera);
+	enemy.SetCamera(camera);
+}
+
+void GameScene::CheckHitAll() {
+
+	// プレイヤーの弾とエネミーの弾
+	for (int i = 0; i < player.kBulletMax; i++) {
+		for (int j = 0; j < enemy.kBulletMax; j++) {
+			if (player.bullets[i].isActive) {
+				if (enemy.bullets[j].isActive) {
+					if (Collision::BoxToBox(
+					        enemy.bullets[j].transform.position, enemy.bullets[j].width, enemy.bullets[j].height, player.bullets[i].transform.position, player.bullets[i].width,
+					        player.bullets[i].height)) {
+						player.bullets[i].Deactive();
+					}
+				}
+			}
+		}
+	}
+
+	// プレイヤーの弾とエネミー
+	for (int i = 0; i < player.kBulletMax; i++) {
+		if (player.bullets[i].isActive) {
+			if (Collision::BoxToBox(enemy.transform.position, enemy.radius, enemy.radius, player.bullets[i].transform.position, player.bullets[i].width, player.bullets[i].height)) {
+				enemy.TakeDamage(1);
+				player.bullets[i].Deactive();
+			}
+		}
+	}
+}
---
+++ TenDaysGameJam25A/GameScene.h
@@ -0,0 +1,56 @@
+﻿#pragma once
+#include "Dlib.h"
+#include "Player.h"
+#include "Enemy.h"
+
+class GameScene {
+public:
+	GameScene();
+
+	/// <summary>
+	/// 初期化をここに
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 更新処理をここに
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画処理をここに　※constによってこの中での値の変更は禁止されている。
+	/// </summary>
+	void Draw() const;
+
+	// アクセッサ (Set～とか、Get～とか)
+	// ----------------------------------------------
+
+	/// <summary>
+	/// 描画カメラセット
+	/// </summary>
+	/// <param name="camera"></param>
+	void SetCamera();
+
+	// 機能
+	// ----------------------------------------------
+
+	/// <summary>
+	/// 全オブジェクトの当たり判定の取得
+	/// </summary>
+	void CheckHitAll();
+
+	// ----------------------------------------------
+	// メンバ変数
+	// ----------------------------------------------
+
+	// システム
+	//-------------------------------------
+	Input input;
+	Transform2D camera;
+	DimensionState dimensionState;
+	Renderer renderer;
+
+	Player player;
+	ParryState parryState;
+	Enemy enemy;
+};
